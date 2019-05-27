2e746c3c6a6e9cd2aa1f85bbce31f82438354585
Mikhaylov Artyom <SimpleRick@yandex.ru>
2019-05-27 20:25:59 +0300

    Fixed division bug

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2017 Mikhaylov Artyom "SimpleRick".
 
-/*  
+/*
 d1 - width of column (default),
 d2 - height of row,
 c1 - number of columns,
@@ -16,20 +16,20 @@ minfs - minimal font size,
 maxfs - max font size,
 
 
-Calculations and ideas: 
-d1*c1 = a*w, d2*c2 = h, c1*c2 > N. 
+Calculations and ideas:
+d1*c1 = a*w, d2*c2 = h, c1*c2 > N.
 Want: d1 ~ d2 in menu-open state ==> d1=d2=d; (d1<=d2) ==> c1 = min integral > sqrt(Naw/h), c2 = min integral > N/c1.
 l^2 = k*d1*d2=k*a*w*h/c1/c2.
  */
 
 
 var w = $("body").width(); //1366
-var h = $("body").height(); 
+var h = $("body").height();
 var a = 0.72;
 var k = 0.15;
 var minfs = 13;
 var maxfs = 15;
-var GridAnimation = true; 
+var GridAnimation = true;
 var Caption = false;
 var BgCaption = true;
 var ListAnimation = true;
@@ -59,7 +59,7 @@ function OpenMenu(Bookmark){
 		} else {
 			for(i = 0; i < Bookmark.children.length; i++) {
 				$("div.menu > div.list").append("<div class = 'favicon'> <img  src='chrome://favicon/size/16@1x/"+ Bookmark.children[i].url +"'> <p><a href='"+ Bookmark.children[i].url+"'>" + Bookmark.children[i].title + "</a></p> </div>");
-			}			
+			}
 		}
 
 	}
@@ -78,24 +78,24 @@ function CreateHtml(Tree) {
 	var l = Math.ceil(Math.sqrt(k*d1*h/c2));
 
 function DrawContextMenu(e) {
-	$("cx-menu").css("visibility", "visible"); 
+	$("cx-menu").css("visibility", "visible");
 	$("cx-menu").css({left:e.pageX, top:e.pageY});
 	var i = ($(e.target).parent()).attr("id");
 	$("cx-menu").append("<button role='menuitem'>"+"Lorem Ipsum"+"</menuitem>");
 	$("cx-menu").append("<button role='menuitem'>"+"Create shortcuts..."+"</menuitem>");
-	
+
 }
 
 	if (Number(0.2*l)<minfs) { Caption = false; }
 
-	for (i = 0;  i < N; i++) {			
-		if(!Bookmarks[i].children) {			
+	for (i = 0;  i < N; i++) {
+		if(!Bookmarks[i].children) {
 			var str = "<figure class='item' id = '"+i+"'> <img class='bookmark' src='icon/"+ (new URL(Bookmarks[i].url)).hostname +".svg' title ='"+ Bookmarks[i].title +"'></figure>";
 			$("div.container").append(str);
 			$("#"+i).click(Transition(Bookmarks[i]));
 			$("#"+i).bind("contextmenu", function(e) {
 				DrawContextMenu(e);
-				e.preventDefault(); 
+				e.preventDefault();
 			});
 		}
 		else {
@@ -104,7 +104,7 @@ function DrawContextMenu(e) {
 			$("#"+i).click(OpenMenu(Bookmarks[i]));
 			$("#"+i).bind("contextmenu", function(e) {
 				DrawContextMenu(e);
-				e.preventDefault(); 
+				e.preventDefault();
 			});
 		}
 	}
@@ -125,7 +125,7 @@ function DrawContextMenu(e) {
 	$("figure.item > img").css("height", l);
 
 	$(".plus-btn").click(function () {
-		$("div.menu > div.header > h, div.menu > div.list > div").detach();	
+		$("div.menu > div.header > h, div.menu > div.list > div").detach();
 		$("body.window").removeClass("menu-open");
 	});
 
@@ -134,7 +134,7 @@ function DrawContextMenu(e) {
 			$("div.menu > div.header > h, div.menu > div.list > div").detach();
 			$("body.window").removeClass("menu-open");
 		}
-		if (!$(e.target).is("cx-menu")) 
+		if (!$(e.target).is("cx-menu"))
 			$("cx-menu").css("visibility", "hidden");
 	});
 
@@ -143,25 +143,25 @@ function DrawContextMenu(e) {
 
 
 	if (GridAnimation) {
-	$(".container").css("grid-template-columns", "repeat("+c1+","+ 100/c1 +"%)"); // grid properties 
-	$(".container").css("grid-template-rows", "repeat("+c2+","+ 100/c2 +"%)");
+	$(".container").css("grid-template-columns", "repeat("+c1+",1fr)"); // grid properties
+	$(".container").css("grid-template-rows", "repeat("+c2+",1fr)");
 	$(".container").css("height", "90vh");
 	$(".container").css("margin", "auto");
 	$("figure.item > img").hover(
 		function() {
 			$(this).animate({
-				width: 1.2*l, 
+				width: 1.2*l,
 				height: 1.2*l
 			}, 100);},
 		function() {
 			$(this).stop(true);
 			$(this).animate({
-				width: l, 
+				width: l,
 				height: l
-			}, 100);}		
+			}, 100);}
 	);
 	} else {
-	$(".container").css("grid-template-columns", "repeat("+c1+", auto)"); // grid properties 
+	$(".container").css("grid-template-columns", "repeat("+c1+", auto)"); // grid properties
 	$(".container").css("grid-template-rows", "repeat("+c2+", auto)");
 	}
 
@@ -182,10 +182,3 @@ function DrawContextMenu(e) {
 
 
 chrome.bookmarks.getTree(CreateHtml);
-
-
-
-
-
-
-
